642a4a5e8d6ce5b3322375766ccf4288045ddb0d
mmpotulo28 <mpotulom28@gmail.com>
2023-10-06 00:34:25 +0200

    update puzzle from colors to images

--- puzzle.js
+++ puzzle.js
@@ -1,15 +1,20 @@
 //query for puzzle pecies
-const puzzlePieces = document.querySelectorAll('.puzzle-peices');
 const puzzlePiece = document.querySelectorAll('.puzzle-peice');
 
-//get order pieces
-const orderPieces = document.querySelectorAll('.win-peice');
+//get reference to the puzzle image element
+const puzzleImage = document.querySelector('.puzzle-image');
 
 var id1 = null;
 var id2 = null;
 
 //create array of colors woth 16 diferent colors
 var colors = ["red", "red", "blue", "blue", "green", "green", "yellow", "yellow", "orange", "orange", "purple", "purple", "pink", "pink", "brown", "brown"];
+//create array of puzzle images from online
+var images = [
+    "1.jpg", "5.jpg", "9.jpg", "13.jpg", 
+    "2.jpg", "6.jpg", "10.jpg", "14.jpg", 
+    "3.jpg", "7.jpg", "11.jpg", "15.jpg", 
+    "4.jpg", "8.jpg", "12.jpg", "16.jpg"]
 var pieceColor = '';
 var puzzleOrder = [];
 
@@ -17,34 +22,43 @@ var puzzleOrder = [];
 var pickedColors = [];
 var pieces = true;
 let i = 0
-var color1 = '', color2 = '';
-var id1S = false
+var image1 = '', image2 = '';
+var id1S = false, id2S = false;
 
-//add mouseclick event listener to each puzzle piece
 setPieceColor()
 
 //loop through puzzle pieces and set color for each peice
 function setPieceColor() {
     puzzlePiece.forEach(piece => {
-        //generate random number between 0 and 15
+        //generate random number between 0 and 15 [this will be the index]
         var randomNum = Math.floor(Math.random() * 16);
+
         //if the random number has already been picked, generate another number
         while (pickedColors.includes(randomNum)) {
             randomNum = Math.floor(Math.random() * 16);
         }
         //push the random number to the pickedColors array
-        pickedColors.push(randomNum);
+        pickedColors.push(randomNum); //this will be used to avoid picking the same color twice
+
         //set the color of the puzzle piece to the color at the index of the random number
-        piece.style.backgroundColor = colors[randomNum];
+        // piece.style.backgroundColor = colors[randomNum];
+
+        //set the background image of the puzzle piece to the image at the index of the random number
+        var imgPath = "./images/puzzle1/" + images[randomNum];
+        piece.style.backgroundImage = "url(" + imgPath + ")";
+        i++
+
     });
 }
 
-//set order pieces to the same color as the color array in order
-orderPieces.forEach(piece => {
-    piece.style.backgroundColor = colors[i];
-    i++;
-});
+//set puzzleImage to the image at the puzzle board
+puzzleImage.style.background = "url(./images/puzzle1/puzzle1.jpeg)";
+puzzleImage.style.backgroundSize = "cover";
 
+
+
+
+//add mouseclick event listener to each puzzle piece
 puzzlePiece.forEach(piece => {
     piece.addEventListener('click', (e) => {
         //if the first piece is clicked, set id1 to the id of the first piece
@@ -57,64 +71,106 @@ puzzlePiece.forEach(piece => {
             document.getElementById(id1).style.borderRadius = "10px";
             document.getElementById(id1).style.transform = "scale(1.15)";
             document.getElementById(id1).style.boxShadow = "0px 0px 10px white";
-            //rotate the piece by 15 degrees
-            //document.getElementById(id1).style.transform = "rotate(15deg)";
 
-            id1S = true;
-        } else {
+            id1S = true; //indicate that the first piece has been selected
+            id2S = false; //indicate that the second piece has not been selected
+        } else if (id2S == false) { //if the second piece is clicked, set id2 to the id of the second piece
             id2 = e.target.id;
             console.log(id2);
-            id1S = false;
+
+            //indicate selected piece by adding border and scaling up
+            document.getElementById(id2).style.border = "2px solid white";
+            document.getElementById(id2).style.borderRadius = "10px";
+            document.getElementById(id2).style.transform = "scale(1.15)";
+            document.getElementById(id2).style.boxShadow = "0px 0px 10px white";
         }
 
         //if both ids are set, swap the colors of the two pieces
         if (id1 != null && id2 != null) {
-            //apply efect on both pieces
-            document.getElementById(id1).style.border = "2px solid white";
-            document.getElementById(id1).style.borderRadius = "50px";
-            document.getElementById(id1).style.transform = "scale(0)";
-            document.getElementById(id2).style.border = "2px solid white";
-            document.getElementById(id2).style.borderRadius = "50px";
-            document.getElementById(id2).style.transform = "scale(0)";
-
-            //get the color of the first piece
-            color1 = document.getElementById(id1).style.backgroundColor;
-            //get the color of the second piece
-            color2 = document.getElementById(id2).style.backgroundColor;
-
-            //delay by 3 seconds before removing border and scaling down
-            setTimeout(() => {
-                //set the color of the first piece to the color of the second piece
-                document.getElementById(id1).style.backgroundColor = color2;
-                //set the color of the second piece to the color of the first piece
-                document.getElementById(id2).style.backgroundColor = color1;
-                //remove border and scale down
-                document.getElementById(id1).style.border = "none";
+            //check if the id2 is the same as id1
+            if (id1 == id2) {
+                //reset styling for 1st piece
+                document.getElementById(id1).style.border = "2px solid black";
                 document.getElementById(id1).style.borderRadius = "0px";
-                document.getElementById(id2).style.border = "none";
-                document.getElementById(id2).style.borderRadius = "0px";
                 document.getElementById(id1).style.transform = "scale(1.0)";
-                document.getElementById(id2).style.transform = "scale(1.0)";
                 document.getElementById(id1).style.boxShadow = "none";
-
-                //set puzzleOrder array to the colors of the puzzle pieces
-                puzzleOrder = [];
-                puzzlePiece.forEach(piece => {
-                    puzzleOrder.push(piece.style.backgroundColor);
-                })
-
-                //compare puzzleOrder array to colors array
-                //if they are the same, alert the user that they won
-                if (puzzleOrder.toString() == colors.toString()) {
-                    alert("You won!");
-                    return;
-                }
-
 
                 id1 = null;
                 id2 = null;
-            }, 500);
+
+                //reset id selection variables
+                id1S = false;
+                id2S = true; //listen for the 1st piece to be selected again
+            } else {
+                checkSwap();
+            }
         }
     });
 });
 
+
+//create a function thta checks if the 2nd selected piece is next to the 1st one
+function checkSwap() {
+    //check if the 2nd selected piece is next to the 1st one
+    if (id1 == id2 - 1 || id1 == id2 + 1 || id1 == id2 - 4 || id1 == id2 + 4) {
+        //if the 2nd selected piece is next to the 1st one, swap the colors of the two pieces
+        //apply efect on both pieces
+        document.getElementById(id1).style.border = "2px solid white";
+        document.getElementById(id1).style.borderRadius = "50px";
+        document.getElementById(id1).style.transform = "scale(0)";
+        document.getElementById(id2).style.border = "2px solid white";
+        document.getElementById(id2).style.borderRadius = "50px";
+        document.getElementById(id2).style.transform = "scale(0)";
+
+        //get the color of the first piece
+        image1 = document.getElementById(id1).style.backgroundImage;
+        //get the color of the second piece
+        image2 = document.getElementById(id2).style.backgroundImage;
+
+        //delay by 3 seconds before removing border and scaling down
+        setTimeout(() => {
+            //set the color of the first piece to the color of the second piece
+            document.getElementById(id1).style.backgroundImage = image2;
+            //set the color of the second piece to the color of the first piece
+            document.getElementById(id2).style.backgroundImage = image1;
+
+            //remove border and scale down
+            document.getElementById(id1).style.border = "2px solid black";
+            document.getElementById(id1).style.borderRadius = "0px";
+            document.getElementById(id2).style.border = "2px solid black";
+            document.getElementById(id2).style.borderRadius = "0px";
+            document.getElementById(id1).style.transform = "scale(1.0)";
+            document.getElementById(id2).style.transform = "scale(1.0)";
+            document.getElementById(id1).style.boxShadow = "none";
+            document.getElementById(id2).style.boxShadow = "none";
+
+            id1 = null;
+            id2 = null;
+
+            //set puzzleOrder array to the colors of the puzzle pieces
+            puzzleOrder = [];
+            puzzlePiece.forEach(piece => {
+                puzzleOrder.push(piece.style.backgroundImage);
+            })
+
+            //compare puzzleOrder array to colors array
+            //if they are the same, alert the user that they won
+            if (puzzleOrder.toString() == images.toString()) {
+                alert("You won!");
+                return;
+            }
+
+            //reset id selection variables
+            id1S = false;
+            id2S = true; //indicate that the second piece has been selected
+
+
+        }, 300);
+    } else {
+        //reset styling for 2nd piece
+        document.getElementById(id2).style.border = "2px solid black";
+        document.getElementById(id2).style.borderRadius = "0px";
+        document.getElementById(id2).style.transform = "scale(1.0)";
+        document.getElementById(id2).style.boxShadow = "none";
+    }
+}
