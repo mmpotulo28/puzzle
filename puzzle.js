456d1a3c148c4052d1daa0d5bdba3b6a4a6e8296
mmpotulo28 <mpotulom28@gmail.com>
2023-10-06 13:55:07 +0200

    add more puzzles

--- puzzle.js
+++ puzzle.js
@@ -1,64 +1,71 @@
 //query for puzzle pecies
 const puzzlePiece = document.querySelectorAll('.puzzle-peice');
+//puzzle container
+const puzzleBoard = document.querySelector('.puzzle_board');
 
 //get reference to the puzzle image element
 const puzzleImage = document.querySelector('.puzzle-image');
+//reference to the win popup container
+const winPopup = document.querySelector('.win-container');
 
 var id1 = null;
 var id2 = null;
 
-//create array of colors woth 16 diferent colors
-var colors = ["red", "red", "blue", "blue", "green", "green", "yellow", "yellow", "orange", "orange", "purple", "purple", "pink", "pink", "brown", "brown"];
-//create array of puzzle images from online
+//create array of puzzle images
 var images = [
-    "1.jpg", "5.jpg", "9.jpg", "13.jpg", 
-    "2.jpg", "6.jpg", "10.jpg", "14.jpg", 
-    "3.jpg", "7.jpg", "11.jpg", "15.jpg", 
+    "1.jpg", "5.jpg", "9.jpg", "13.jpg",
+    "2.jpg", "6.jpg", "10.jpg", "14.jpg",
+    "3.jpg", "7.jpg", "11.jpg", "15.jpg",
     "4.jpg", "8.jpg", "12.jpg", "16.jpg"]
-var pieceColor = '';
-var puzzleOrder = [];
 
-//array to log which picked colors to avoid picked duplicates
-var pickedColors = [];
-var pieces = true;
+var puzzles = ['puzzle1.jpeg', 'puzzle2.jpg', 'puzzle3.jpg']
+var puzzleFolders = ['puzzle1', 'puzzle2', 'puzzle3']
+var puzzleFolder = '' //to be used to store the name of the picked puzzle folder
+var random = 0; //to be used to store the random number
+
+var imageOrder = []; //to be used to check the order of the puzzle pieces
+var picked = []; //to be used to avoid picking the same color twice
 let i = 0
-var image1 = '', image2 = '';
-var id1S = false, id2S = false;
+var image1 = '', image2 = '';// to store current images for the pieces
+var id1S = false, id2S = false; //to indicate if a specific piece is selected
 
-setPieceColor()
+setPuzzle();
 
 //loop through puzzle pieces and set color for each peice
-function setPieceColor() {
+function setPuzzle() {
+
+    //pick puzzle folder
+    random = Math.floor(Math.random() * 3);
+    puzzleFolder = puzzleFolders[random].toString();
+
     puzzlePiece.forEach(piece => {
         //generate random number between 0 and 15 [this will be the index]
         var randomNum = Math.floor(Math.random() * 16);
 
         //if the random number has already been picked, generate another number
-        while (pickedColors.includes(randomNum)) {
+        while (picked.includes(randomNum)) {
             randomNum = Math.floor(Math.random() * 16);
         }
-        //push the random number to the pickedColors array
-        pickedColors.push(randomNum); //this will be used to avoid picking the same color twice
-
-        //set the color of the puzzle piece to the color at the index of the random number
-        // piece.style.backgroundColor = colors[randomNum];
+        //push the random number to the picked array
+        picked.push(randomNum); //this will be used to avoid picking the same color twice
 
         //set the background image of the puzzle piece to the image at the index of the random number
-        var imgPath = "./images/puzzle1/" + images[randomNum];
+        let imgPath = "./images/" + puzzleFolder + "/" + images[randomNum];
+        // alert(imgPath)
         piece.style.backgroundImage = "url(" + imgPath + ")";
-        i++
+        // i++
 
     });
+
+    //set puzzleImage to the image at the puzzle board
+    puzzleImage.style.background = "url(./images/" + puzzleFolder + "/" + puzzles[random] + ")";
+    puzzleImage.style.backgroundSize = "100% 100%";
+
 }
 
-//set puzzleImage to the image at the puzzle board
-puzzleImage.style.background = "url(./images/puzzle1/puzzle1.jpeg)";
-puzzleImage.style.backgroundSize = "cover";
 
 
-
-
-//add mouseclick event listener to each puzzle piece
+//add mouseclick event listener to each puzzle piece click event
 puzzlePiece.forEach(piece => {
     piece.addEventListener('click', (e) => {
         //if the first piece is clicked, set id1 to the id of the first piece
@@ -69,8 +76,10 @@ puzzlePiece.forEach(piece => {
             //indicate selected piece by adding border and scaling up
             document.getElementById(id1).style.border = "2px solid white";
             document.getElementById(id1).style.borderRadius = "10px";
-            document.getElementById(id1).style.transform = "scale(1.15)";
+            document.getElementById(id1).style.transform = "scale(1.2)";
             document.getElementById(id1).style.boxShadow = "0px 0px 10px white";
+            //bring upfront
+            document.getElementById(id1).style.zIndex = "1";
 
             id1S = true; //indicate that the first piece has been selected
             id2S = false; //indicate that the second piece has not been selected
@@ -81,9 +90,11 @@ puzzlePiece.forEach(piece => {
             //indicate selected piece by adding border and scaling up
             document.getElementById(id2).style.border = "2px solid white";
             document.getElementById(id2).style.borderRadius = "10px";
-            document.getElementById(id2).style.transform = "scale(1.15)";
+            document.getElementById(id2).style.transform = "scale(1.2)";
             document.getElementById(id2).style.boxShadow = "0px 0px 10px white";
         }
+
+
 
         //if both ids are set, swap the colors of the two pieces
         if (id1 != null && id2 != null) {
@@ -147,16 +158,24 @@ function checkSwap() {
             id1 = null;
             id2 = null;
 
-            //set puzzleOrder array to the colors of the puzzle pieces
-            puzzleOrder = [];
+            //set puzzleOrder array to the images of the puzzle pieces
+            imageOrder = [];
             puzzlePiece.forEach(piece => {
-                puzzleOrder.push(piece.style.backgroundImage);
+                //get name of the background image of the puzzle piece
+                let imgName = piece.style.backgroundImage;
+
+                //clean the name of the image to get the name of the image only
+                imgName = imgName.replace('url("', "");
+                imgName = imgName.replace('")', "");
+                imgName = imgName.replace('./images/' + [puzzleFolder] + '/', "");
+                //push the name to the imageOrder array
+                imageOrder.push(imgName);
             })
 
-            //compare puzzleOrder array to colors array
-            //if they are the same, alert the user that they won
-            if (puzzleOrder.toString() == images.toString()) {
-                alert("You won!");
+
+            // alert(imageOrder)
+            if (imageOrder.toString() == images.toString()) {
+                winLoose("block");
                 return;
             }
 
@@ -174,3 +193,26 @@ function checkSwap() {
         document.getElementById(id2).style.boxShadow = "none";
     }
 }
+function winLoose(dis) {
+
+    //set pop up display
+    winPopup.style.display = dis;
+
+    //if winpopup is visible, blur the background of board only
+    if (winPopup.style.display == "none") {
+        puzzleBoard.style.opacity = "1";
+    } else if (winPopup.style.display == "block") {
+        puzzleBoard.style.opacity = "0.2";
+    }
+}
+
+//add click event listener to the play again button
+const playAgainBtn = document.querySelector('#play-again');
+playAgainBtn.addEventListener('click', () => {
+    //hide the win popup
+    winLoose("none");
+
+    //reset the puzzle pieces
+    setPuzzle();
+
+})
